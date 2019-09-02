a19a050e7ea206b3b2a150bc358c364b096bb4bb
KimuTsuri <ktsuri@hpx.phys.ocha.ac.jp>
2019-09-02 22:11:55 +0900

    modified:   first/.DS_Store 	modified:   first/myapp/__pycache__/urls.cpython-37.pyc 	modified:   first/myapp/__pycache__/views.cpython-37.pyc 	new file:   first/myapp/templates/.DS_Store 	new file:   first/myapp/templates/myapp/about.html 	new file:   first/myapp/templates/myapp/base.html 	new file:   first/myapp/templates/myapp/index.html 	new file:   first/myapp/templates/myapp/info.html 	modified:   first/myapp/urls.py 	modified:   first/myapp/views.py 	new file:   project/db.sqlite3 	new file:   project/diary/.idea/.gitignore 	new file:   project/diary/.idea/diary.iml 	new file:   project/diary/.idea/inspectionProfiles/profiles_settings.xml 	new file:   project/diary/.idea/misc.xml 	new file:   project/diary/.idea/modules.xml 	new file:   project/diary/.idea/vcs.xml 	new file:   project/diary/__init__.py 	new file:   project/diary/__pycache__/__init__.cpython-37.pyc 	new file:   project/diary/__pycache__/admin.cpython-37.pyc 	new file:   project/diary/__pycache__/apps.cpython-37.pyc 	new file:   project/diary/__pycache__/forms.cpython-37.pyc 	new file:   project/diary/__pycache__/models.cpython-37.pyc 	new file:   project/diary/__pycache__/urls.cpython-37.pyc 	new file:   project/diary/__pycache__/views.cpython-37.pyc 	new file:   project/diary/admin.py 	new file:   project/diary/apps.py 	new file:   project/diary/forms.py 	new file:   project/diary/migrations/0001_initial.py 	new file:   project/diary/migrations/__init__.py 	new file:   project/diary/migrations/__pycache__/0001_initial.cpython-37.pyc 	new file:   project/diary/migrations/__pycache__/__init__.cpython-37.pyc 	new file:   project/diary/models.py 	new file:   project/diary/templates/.DS_Store 	new file:   project/diary/templates/diary/base.html 	new file:   project/diary/templates/diary/day_form.html 	new file:   project/diary/templates/diary/day_list.html 	new file:   project/diary/tests.py 	new file:   project/diary/urls.py 	new file:   project/diary/views.py 	new file:   project/manage.py 	new file:   project/project/__init__.py 	new file:   project/project/__pycache__/__init__.cpython-37.pyc 	new file:   project/project/__pycache__/settings.cpython-37.pyc 	new file:   project/project/__pycache__/urls.cpython-37.pyc 	new file:   project/project/__pycache__/wsgi.cpython-37.pyc 	new file:   project/project/settings.py 	new file:   project/project/urls.py 	new file:   project/project/wsgi.py

--- first/myapp/views.py
+++ first/myapp/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World")
+    context = {
+        'name':'Kimu',
+    }
+    return render(request, 'myapp/index.html', context)
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def info(request):
+    return render(request, 'myapp/info.html')
