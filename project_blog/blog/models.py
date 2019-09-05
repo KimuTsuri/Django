30245df6bfc3c424f9876e09422c21bd06a1d7cc
KimuTsuri <ktsuri@hpx.phys.ocha.ac.jp>
2019-09-05 14:54:18 +0900

    new file

--- project_blog/blog/models.py
+++ project_blog/blog/models.py
@@ -11,7 +11,20 @@ class Post(models.Model):
     title = models.CharField('タイトル', max_length=150)
     text = models.TextField('本文')
     created_at = models.DateTimeField('作成日', default=timezone.now)
-    category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+    category = models.ForeignKey(
+        Category, verbose_name='カテゴリ', on_delete=models.PROTECT
+    )
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    name = models.CharField('お名前', max_length=30, default='none name')
+    text = models.TextField('本文')
+    post = models.ForeignKey(
+        Post, verbose_name='コメントする記事', on_delete=models.PROTECT
+    )
+    created_at = models.DateTimeField('コメント日', default=timezone.now)
+
+    def __str__(self):
+        return self.text[:10]
